870f7453069db30002924e1801d7129978cf103c
MANTAS TAMASAUSKAS <c0029478@hallam.shu.ac.uk>
2020-11-19 21:47:58 +0000

    Add project files.

---
+++ Models/Admins.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_Project.Models
+{
+    public class Admins
+    {
+        public int AdminID { get; set; } //Unique ID
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string AdminName { get; set; } // can be displayed throughout the website, e.g in index "Welsome Zairul"
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string AdminGamil { get; set; } // For Login
+
+        [Required]
+        [Display(Name = "Password")]
+        public string MemberPassword { get; set; } // For login 
+
+    }
+}
---
+++ Models/Memebers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_Project.Models
+{
+    public class Memebers
+    {
+
+        public int MemberID { get; set; } //Unique ID
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string MemberName { get; set; } // can be displayed throughout the website, e.g in index "Welsome Zairul"
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string MemberGamil { get; set; } // For login 
+
+        [Required]
+        [Display(Name = "Password")]
+        public string MemberPassword { get; set; } // For login 
+
+        [Required]
+        [Display(Name = "Membership")]
+        public string Membership { get; set; } // Free | Basic  £3.99 | Full £8.99 | *Only one can be selected*
+
+                                               // Free - 1-2 Playlists 
+                                               // Basic - 3-5 Playlists
+                                               // Full - all available playlists
+
+
+    }
+}
